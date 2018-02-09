901138a24c76de7e479cd7b967e6429082dfa18f
Arit Amana <>
2018-02-09 13:36:50 -0500

    ""

---
+++ gitworkflow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script executes the Git Workflow
+
+echo "Type your commit message:"
+echo "(no quotation marks)"
+read commitmsg
+
+if [ $commitmsg == "" ]; then
+	echo "You did not include a commit message"
+	sleep 1
+	echo "Exiting script now..."
+	sleep 1
+	exit
+else
+	git add --all
+	git commit -am \"$commitmsg\"
+	git push
+
+fi
