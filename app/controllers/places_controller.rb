43dbc15e72c903d7ee93130c8c7707d4c6ebd41e
Arit Amana <>
2018-02-17 13:47:22 -0500

    Add validation for Add Place - Name Field

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,32 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-    
+
+
     def index
       @places = Place.order("name").page(params[:page]).per_page(5)
     end
+
 
     def new
       @place = Place.new
       end
 
+
     def create
-      current_user.places.create(place_params)
-      redirect_to root_path
+      @place = current_user.places.create(place_params)
+
+        if @place.valid?
+          redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
     end
+
 
     def show
       @place = Place.find(params[:id])
     end
+
 
     def edit
       @place = Place.find(params[:id])
@@ -24,8 +34,8 @@ class PlacesController < ApplicationController
       if @place.user != current_user
         return render plain: 'Not Allowed!', status: :forbidden
       end
-
     end
+
 
     def update
       @place = Place.find(params[:id])
@@ -35,8 +45,14 @@ class PlacesController < ApplicationController
       end
       
       @place.update_attributes(place_params)
-      redirect_to place_path(@place)
+
+      if @place.valid?
+        redirect_to root_path
+      else
+          render :edit, status: :unprocessable_entity
+      end
     end
+
 
     def destroy
       @place = Place.find(params[:id])
@@ -44,10 +60,11 @@ class PlacesController < ApplicationController
       if @place.user != current_user
         return render plain: 'Not Allowed!', status: :forbidden
       end
-      
+
       @place.destroy
       redirect_to root_path
     end
+
 
     private
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
 class Place < ApplicationRecord
   belongs_to :user
+  validates :name, presence: { message: "The NAME of the place is required." }
 end
