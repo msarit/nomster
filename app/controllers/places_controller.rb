ca9928de0e9e97cc7db9179a86a611da4afcc75a
Arit Amana <>
2018-02-17 11:52:59 -0500

    Configure Edit-Place Action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
     
     def index
-      @places = Place.order("RANDOM()").page(params[:page]).per_page(5)
+      @places = Place.order("name").page(params[:page]).per_page(5)
     end
 
     def new
@@ -20,6 +20,12 @@ class PlacesController < ApplicationController
 
     def edit
       @place = Place.find(params[:id])
+    end
+
+    def update
+      @place = Place.find(params[:id])
+      @place.update_attributes(place_params)
+      redirect_to place_path(@place)
     end
 
     private
