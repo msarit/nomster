0639670ac5ab1de666e86536105b6895c891c53f
Arit Amana <>
2018-02-17 15:35:56 -0500

    Add validations for presence of Place Address, Description; and Name length

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: { message: "The NAME of the place is required." }
+  validates :name, presence: { message: "The NAME of the place is required." }, length: { minimum: 3, too_short: "Name must be at least %{count} characters." }
+  validates :address, presence: { message: "The ADDRESS of the place is required." }
+  validates :description, presence: { message: "The DESCRIPTION of the place is required." }
 end
