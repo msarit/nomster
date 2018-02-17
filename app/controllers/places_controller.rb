7e208d5a3c312bc84203d03bd226af0c28e137d9
Arit Amana <>
2018-02-17 08:41:02 -0500

    Add User Details - Email - to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,23 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+    
+    def index
+      @places = Place.order("RANDOM()").page(params[:page]).per_page(5)
+    end
 
-def index
-  @places = Place.order("RANDOM()").page(params[:page]).per_page(5)
-end
+    def new
+      @place = Place.new
+      end
 
-def new
-  @place = Place.new
-  end
+    def create
+      current_user.places.create(place_params)
+      redirect_to root_path
+    end
 
-def create
-  place.create(place_params)
-  redirect_to root_path
-end
+    private
 
-private
-
-def place_params
-  param.require(:place).permit(:name, :description, :address)
-end
+    def place_params
+      params.require(:place).permit(:name, :description, :address)
+    end
 
 end
