27be55856e78398fc6a50a3221894dd781458d90
Arit Amana <>
2018-02-17 13:04:06 -0500

    Configure app to prevent place-destroy by non-author

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     
     def index
       @places = Place.order("name").page(params[:page]).per_page(5)
@@ -40,6 +40,11 @@ class PlacesController < ApplicationController
 
     def destroy
       @place = Place.find(params[:id])
+
+      if @place.user != current_user
+        return render plain: 'Not Allowed!', status: :forbidden
+      end
+      
       @place.destroy
       redirect_to root_path
     end
