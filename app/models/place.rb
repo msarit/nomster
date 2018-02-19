63448f2c931bfc2e2592bf09ac1d957f2f241b2a
Arit Amana <>
2018-02-19 14:27:21 -0500

    Set up database for Comments feature

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, presence: { message: "The NAME of the place is required." }, length: { minimum: 3, too_short: "Name must be at least %{count} characters." } 
   validates :address, presence: { message: "The ADDRESS of the place is required." }
   validates :description, presence: { message: "The DESCRIPTION of the place is required." }
