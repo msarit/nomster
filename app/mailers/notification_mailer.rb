4ae68b2184b8c078a496aa020c61297c8f806459
Arit Amana <>
2018-02-24 15:12:46 -0500

    Work on Mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
     default from: "no-reply@sscakeries.com"
 
-  def comment_added
-    mail(to: "all@thebreisagency.com",
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
          subject: "A comment has been added to your place")
   end
 end
