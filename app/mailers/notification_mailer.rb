1df006a087a66caa751e39e5d48256aaca00cf66
Arit Amana <>
2018-03-01 10:44:23 -0500

    Made email custom to user, added link to place with new comment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,10 @@
 class NotificationMailer < ApplicationMailer
-    default from: "no-reply@sscakeries.com"
+    default from: "no-reply@sscupcakeries.com"
 
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
     mail(to: @place_owner.email,
-         subject: "A comment has been added to your place")
+         subject: "A comment has been added to #{@place.name}")
   end
 end
