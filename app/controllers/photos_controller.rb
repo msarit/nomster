0a171b0d83051d41360ab02db67ca99da51dcfb2
Arit Amana <>
2018-03-23 16:05:40 -0400

    Cleaning Up

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,11 +4,12 @@ class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params)
-      if @place.photos.create(photo_params).valid?
-        redirect_to place_path(@place)
-      else
+    
+    if @place.photos.create(photo_params).valid?
+      redirect_to place_path(@place)
+    else
       render plain: 'ERROR: No picture attached; click "Back" button to go back', status: :unprocessable_entity
-      end
+    end
   end
 
   private
