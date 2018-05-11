483e482421922c68ce36e54af86961b382118a75
Arit Amana <>
2018-05-11 13:20:13 -0400

    Add dependent-destroy to photos in place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,12 +1,12 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments, :dependent => :delete_all
-  has_many :photos
+  has_many :photos, :dependent => :delete_all
 
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, presence: { message: "The NAME of the place is required." }, length: { minimum: 3, too_short: "Name must be at least %{count} characters." } 
-  validates :address, presence: { message: "The ADDRESS of the place is required." }
-  validates :description, presence: { message: "The DESCRIPTION of the place is required." }
+  validates :name, presence: { message: "The NAME of the place is required." }, length: { minimum: 3, too_short: "Name must be at least %{count} characters.", maximum: 70 } 
+  validates :address, presence: { message: "The ADDRESS of the place is required." }, length: { maximum: 140 }
+  validates :description, presence: { message: "The DESCRIPTION of the place is required." }, length: { minimum: 20, too_short: "Description too short.", maximum: 300 }
 end
