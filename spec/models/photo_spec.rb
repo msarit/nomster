070e8f7f4d3662579feb7a378acb0e9b5077ed48
Arit Amana <>
2018-05-28 21:18:01 -0400

    got rid of specs

--- spec/models/photo_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Photo, type: :model do
-  describe "Factories" do
-    it "will attach a photo" do
-      expect(FactoryBot.build(:photo).picture).to be_present
-    end
-  end
-end
