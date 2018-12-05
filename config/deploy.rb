b6774168c8f8e70f3601c548a967da1020ee5be2
Arit Amana <msarit@gmail.com>
2018-12-05 14:27:04 -0500

    cap production deploy issues 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:msarit/nomster.git"
 
 set :deploy_to, '/home/deploy/sscupcakeries'
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
